a3a9d87bd6bb0dd7f3411cb2d9b8a5bcb35eb6f0
HuyTrinh <huy@makehelsinki.com>
2018-10-29 14:46:09 +0200

    Add test actions

---
+++ src/store/actions/__tests__/user.test.ts
@@ -0,0 +1,49 @@
+import {userActionType} from "../actionTypes";
+import {fetchUsersStart, fetchUsersSuccess, fetchUsersFail} from "../user";
+import {User} from "../../../models/user";
+
+describe("User Actions", () => {
+  describe("fetchUsersStart", () => {
+    it("should return the correct type", () => {
+      const expectedResult = {
+        type: userActionType.FETCH_USER_START,
+      };
+
+      expect(fetchUsersStart()).toEqual(expectedResult);
+    });
+  });
+
+  describe("fetchUsersSuccess", () => {
+    const testUsers: User[] = [
+      {
+        id: 1,
+        username: "Bret",
+        email: "Sincere@april.biz",
+        phone: " 1-770-736-8031 x56442",
+        website: "hildegard.org",
+      },
+    ];
+
+    it("should return the correct type", () => {
+      const expectedResult = {
+        type: userActionType.FEFETCH_USER_SUCCESS,
+        users: testUsers,
+      };
+
+      expect(fetchUsersSuccess(testUsers)).toEqual(expectedResult);
+    });
+  });
+
+  describe("fetchUsersFail", () => {
+    it("should return the correct type", () => {
+      const testError = "error";
+
+      const expectedResult = {
+        type: userActionType.FETCH_USER_FAIL,
+        error: testError,
+      };
+
+      expect(fetchUsersFail(testError)).toEqual(expectedResult);
+    });
+  });
+});
